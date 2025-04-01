dc97072e25af8cbd118e0229401cfe5a1164588f
mariapaulafg18 <maria.paula.f.g18@gmail.com>
2025-03-31 22:38:39 -0500

    delete function, some styles

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
 button=document.getElementById("add");
 input=document.getElementById("input");
-
+list=document.getElementById("task-list");
 
 
 const tasks=[];
 
+button.disabled=true;
+
+ function render(){
+    list.innerHTML='';
+
+    tasks.forEach(task => {
+        let newDiv=document.createElement('div');
+    let delit=document.createElement('button');
+        delit.classList.add('delet')
+        newDiv.classList.add('task-div')
+        delit.textContent="x";
+        const newContent = document.createTextNode(task);
+        newDiv.appendChild(newContent);
+        newDiv.appendChild(delit);
+        list.appendChild(newDiv)
+        delit.addEventListener('click', ()=>{
+        deleteTask()
+        })
+    });
+    
+ }
 
 
 function addTask(){
     tasks.push(input.value)
-    input.value="";
+        button.disabled=true;
+        render()
+        input.value="";
 }
 
+function deleteTask(id){
+    const index=tasks.findIndex(task=> task.id=== id);
 
+    if (index !==-1) {
+        tasks.splice(index,1)
+    }
+    render()
+}
+
+input.addEventListener('input',()=>{
+    if (input.value == "") {
+        button.disabled=true;
+    }else {
+        button.disabled=false;
+    }
+})
 
 
 
 button.addEventListener('click',()=>{
     addTask()
-    // for (let index = 0; index < tasks.length; index++) {
-    //     console.log(tasks[index])
-        
-    // }
 })
