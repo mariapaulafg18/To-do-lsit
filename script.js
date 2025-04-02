de43d6204dd34451566ec3ac39c033d5c1d97776
mariapaulafg18 <maria.paula.f.g18@gmail.com>
2025-04-01 21:57:08 -0500

    edit tasks functionality

--- script.js
+++ script.js
@@ -4,45 +4,64 @@ list=document.getElementById("task-list");
 
 
 const tasks=[];
+initialIndex=-1;
 
 button.disabled=true;
 
  function render(){
     list.innerHTML='';
 
-    tasks.forEach(task => {
+    tasks.forEach((task, index)=> {
         let newDiv=document.createElement('div');
     let delit=document.createElement('button');
+    let edit=document.createElement('button')
+     edit.classList.add('edit')
+     edit.textContent='Edit'
         delit.classList.add('delet')
         newDiv.classList.add('task-div')
         delit.textContent="x";
         const newContent = document.createTextNode(task);
         newDiv.appendChild(newContent);
+        newDiv.appendChild(edit)
         newDiv.appendChild(delit);
         list.appendChild(newDiv)
         delit.addEventListener('click', ()=>{
-        deleteTask()
+        deleteTask(index)
         })
+        edit.addEventListener('click', ()=>{
+            editTask(index)
+            })
     });
-    
+    button.textContent='Add Task';
  }
 
 
 function addTask(){
-    tasks.push(input.value)
+    console.log(initialIndex)
+    const task=input.value.trim()
+    
+    if (initialIndex!==-1) {
+        tasks[initialIndex]=task
+
+    }else{
+    tasks.push(task)
         button.disabled=true;
-        render()
-        input.value="";
+    }
+    input.value="";
+    render()
+    initialIndex=-1;
+
+
 }
 
-function deleteTask(id){
-    const index=tasks.findIndex(task=> task.id=== id);
-
+function deleteTask(index){
     if (index !==-1) {
         tasks.splice(index,1)
     }
     render()
 }
+
+
 
 input.addEventListener('input',()=>{
     if (input.value == "") {
@@ -57,3 +76,10 @@ input.addEventListener('input',()=>{
 button.addEventListener('click',()=>{
     addTask()
 })
+
+
+function editTask(index){
+    initialIndex=index
+    input.value=tasks[index]
+    button.textContent='Update Task';
+}
